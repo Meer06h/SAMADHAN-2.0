e94222dcefbc524463ed2cd1c9838d2376120e1c
Mohd-Ameen987 <149228509+Mohd-Ameen987@users.noreply.github.com>
2025-08-26 00:03:34 +0530

    script.js

--- Day 02/script.js
+++ Day 02/script.js
@@ -1,10 +1,7 @@
-let marks=[45,67,87,34,23,79];
-let highest=0,i;
-for(i=0;i<marks.length;i++)
-{
-    if(marks[i]>highest)
-    {
-        highest=marks[i];
-    }
+function findHighest() {
+    
+    let marks = [85, 92, 76, 98, 89, 91];
+    let highest = Math.max(...marks);
+    document.getElementById("result").innerText = 
+        "The highest marks are: " + highest;
 }
-console.log("Highest Marks in the Array = "+ highest);
